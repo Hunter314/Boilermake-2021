5ef3503728934a33ea738e9045feb444887394ca
rfheise <43056909+rfheise@users.noreply.github.com>
2021-01-22 23:53:56 -0500

    csvs 😅

--- carz/boiler/csv_reader.py
+++ carz/boiler/csv_reader.py
@@ -1,4 +1,4 @@
 from . import models
+import csv
 
 def t2003():
-    
---
+++ carz/boiler/csvs/csv_graber.py
@@ -0,0 +1,16 @@
+import requests
+
+def main():
+    for i in range(16):
+        string = "{:0>2d}".format(i)
+        r = requests.get(f"https://www.epa.gov/sites/production/files/2016-07/{string}tstcar.csv")
+        if r.status_code == 200:
+            with open(f"./year{i + 1}.csv","w") as f:
+                f.write(r.text)
+
+
+
+
+
+if __name__ == "__main__":
+    main()

cdf572f22ae050baf1ee2d0d5d02917ffb495ed2
rfheise <43056909+rfheise@users.noreply.github.com>
2021-01-23 07:58:20 -0500

    api

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask,jsonify,request
 import time
 import os
 import base64
+from run import testrun
 app = Flask(__name__)
 
 #quick api status generator
@@ -9,17 +10,23 @@ def func(image):
     return "cool"
 def status(status, payload):
     return jsonify({"status":status,"payload":payload})
+
 @app.route('/',methods=["POST"])
 def api():
     #determines if required field is there
     if request.form['image']:
         cwd = os.getcwd()
-        image_path = f"{cwd}/pics/{time.time()}"
+        t = f"{time.time()}"
+        os.mkdir(cwd+"/"+t)
+        directory = f"{cwd}/{t}"
+        image_path = f"{directory}/img"
         #writes image to dummy path
-        with open(f"./pics/{time.time()}","wb") as f:
+        with open(image_path,"wb") as f:
             f.write(base64.b64decode(request.form['image']))
         #passes image path to image processor
-        data = func(image_path)
+        data = str(testrun(directory))
+        os.remove(image_path)
+        os.rmdir(directory)
         #returns data
         return status(True, data)
     else:

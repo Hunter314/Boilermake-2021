b6d482587d31d2651c304e907d91419eb39d44e0
rfheise <43056909+rfheise@users.noreply.github.com>
2021-01-22 23:35:33 -0500

    pusher

---
+++ carz/boiler/csv_reader.py
@@ -0,0 +1,4 @@
+from . import models
+
+def t2003():
+    
---
+++ carz/boiler/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Car(models.Model):
+    model = models.TextField(default = "")
+    make = models.TextField(default = "")
+    year = models.IntegerField(default = 0)
+    #carbon dioxide
+    co2 = models.FloatField(null = True,default = None)
+    #carbon monoxide
+    co = models.FloatField(null = True,default = None)
+    #nitrous oxide
+    n2o = models.FloatField(null = True,default = None)
